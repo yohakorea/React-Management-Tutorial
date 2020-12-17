aab090ca68ec86c6f6ddb293a6c078225096f017
yohakorea <yohakorea@gmail.com>
2020-12-18 05:59:37 +0900

    Create React Project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="gray-background">
+      <img src={logo} alt="logo" />
+      <h2>리액트로 개발해보자</h2>
+    </div>
+  );
+}
+
+export default App;
