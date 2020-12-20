64244dd5a15688037015d8020daffecdb1c5dbc5
yohakorea <yohakorea@gmail.com>
2020-12-20 16:42:53 +0900

    Node.js Database Connection Setting

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -6,33 +7,26 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: conf.host,
+  user: conf.user,
+  password: conf.password,
+  port: conf.port,
+  database: conf.database
+});
+connection.connect();
+
 app.get('/api/customers', (req, res) => {
-    res.send([
-        {
-            'id': 1,
-            'image': 'https://placeimg.com/64/64/1',
-            'name': '황용하',
-            'birthday': '701111',
-            'gender': '남자',
-            'job': '사장'
-          },
-          {
-            'id': 2,
-            'image': 'https://placeimg.com/64/64/2',
-            'name': '황길동',
-            'birthday': '843245',
-            'gender': '남자',
-            'job': '대학생'
-          },
-          {
-            'id': 3,
-            'image': 'https://placeimg.com/64/64/3',
-            'name': '고둘리',
-            'birthday': '943245',
-            'gender': '남자',
-            'job': '알바'
-          }        
-    ]);
+  connection.query(
+    "SELECT * FROM CUSTOMER",
+    (err, rows, fields) => {
+      res.send(rows);
+    }
+  );    
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
